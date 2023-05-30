2f591f5f63643a601b6ba2024cb61f63256d0bfc
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-05-30 22:15:08 +0200

    1

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,14 @@
-window.addEventListener('load', function () {
-    let firstBackground = document.getElementById('greetBackground');
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
+window.addEventListener(`load`, function () {
+    let firstBackground = document.getElementById(`greetBackground`);
+    let canvas = document.getElementById(`canvas`);
+    let ctx = canvas.getContext(`2d`);
     let fontSize = 5;
     let columns = canvas.width / fontSize;
     let drops = [];
-    let letters = '｡ ｢ ｣ ､ ･ ｦ ｧ ｨ ｩ ｪ ｫ ｬ ｭ ｮ ｯｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍﾏﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ﾝ ﾞ ﾟ';
+    let letters = `｡ ｢ ｣ ､ ･ ｦ ｧ ｨ ｩ ｪ ｫ ｬ ｭ ｮ ｯｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍﾏﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ﾝ ﾞ ﾟ`;
 
     setTimeout(function () {
-        firstBackground.style.display = 'none';
+        firstBackground.style.display = `none`;
         for (let i = 0; i < columns; i++) {
             drops[i] = Math.floor(Math.random() * canvas.height);
         }
@@ -17,15 +17,15 @@ window.addEventListener('load', function () {
         let greetingDisplayed = false;
 
         function draw() {
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+            ctx.fillStyle = `rgba(0, 0, 0, 0.05)`;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
             for (let i = 0; i < drops.length; i++) {
                 let text = letters[Math.floor(Math.random() * letters.length)];
 
-                ctx.fillStyle = '#0f0';
+                ctx.fillStyle = `#0f0`;
 
-                ctx.font = fontSize + 'px Arial';
+                ctx.font = fontSize + `px Arial`;
 
                 ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
@@ -39,17 +39,17 @@ window.addEventListener('load', function () {
             }
 
             if (counter >= 50 && !greetingDisplayed) {
-                let greeting = document.createElement('div');
-                greeting.id = 'greeting';
+                let greeting = document.createElement(`div`);
+                greeting.id = `greeting`;
 
                 document.body.appendChild(greeting);
                 function sayHello() {
                     let text =
-                        'console.log ("HELLO WORLD");' + ' ' + 'Click anywhere on the screen to let me introduce myself!';
+                        `console.log ('HELLO WORLD');` + ` ` + `Click anywhere on the screen to let me introduce myself!`;
 
-                    let textArray = text.split('');
+                    let textArray = text.split(``);
                     let i = 0;
-                    let greeting = document.getElementById('greeting');
+                    let greeting = document.getElementById(`greeting`);
                     let intervalId = setInterval(function () {
                         greeting.innerHTML += textArray[i];
                         i++;
@@ -71,40 +71,68 @@ window.addEventListener('load', function () {
         requestAnimationFrame(draw);
     }, 3000);
 });
-window.addEventListener('click', function () {
-    let greeting = document.getElementById('greeting');
-    let secondBackground = document.getElementById('frame');
+window.addEventListener(`click`, function () {
+    let greeting = document.getElementById(`greeting`);
+    let secondBackground = document.getElementById(`frame`);
+    let canvas = document.getElementById(`canvas`);
 
     canvas.remove();
     greeting.remove();
-    secondBackground.style.setProperty('display', 'block');
+    secondBackground.style.setProperty(`display`, `block`);
     document.body.appendChild(secondBackground);
 });
-const form = document.getElementById('contactForm');
+const form = document.querySelector('form');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const subject = document.querySelector('#subject').value;
+    const message = document.querySelector('#message').value;
 
-    const messageBody = [
-        "From: " + name + " <" + email + ">",
-        "To: jasenkobozinovic@gmail.com",
-        "Subject: " + subject,
-        "",
-        message
-    ].join("\n");
-
-    const encodedMessage = btoa(messageBody);
-    const request = gapi.client.gmail.users.messages.send({
-        'userId': 'me',
-        'resource': {
-            'raw': encodedMessage
-        }
+    const headers = new Headers({
+        'Content-Type': 'application/json'
     });
 
-    request.execute(() => alert('Thank you for your message!'));
+    const body = JSON.stringify({
+        personalizations: [
+            {
+                to: [
+                    {
+                        email: 'jasenkobozinovic@gmail.com'
+                    }
+                ]
+            }
+        ],
+        from: {
+            email: email,
+            name: name
+        },
+        subject: subject,
+        content: [
+            {
+                type: 'text/plain',
+                value: message
+            }
+        ]
+    });
+
+    fetch('https://api.sendgrid.com/v3/mail/send', {
+        method: 'POST',
+        headers: headers,
+        body: body,
+        Authorization: 'Bearer cY_FJT3aTF-1JSGZiPRP6A '
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Email sent!');
+            } else {
+                alert('Email could not be sent. Error: ' + response.status);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert('Email could not be sent. Error: ' + error.message);
+        });
 });
