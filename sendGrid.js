895a1b956369dfef3efa1bb6eedde839c17b0746
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-06-12 08:37:14 +0200

    1

---
+++ sendGrid.js
@@ -0,0 +1,18 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+    to: 'jasenkobozinovic@gmail.com',
+    from: 'jasenkobozinovic@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+send();
