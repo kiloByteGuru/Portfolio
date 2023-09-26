7dcf7964fa485e6a2a80f161484b1d461299e98d
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-09-26 14:58:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,91 @@
-
-document.addEventListener("scroll", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const projects = document.querySelectorAll(".card.project");
-  projects.forEach((project) => {
-    const projectPosition = project.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.3;
+  const backToTopBtn = document.querySelector("#backToTopBtn");
+  const navLinks = document.querySelectorAll(".nav-link");
+  const form = document.querySelector('.contact-form');
+  const thankYouMessage = document.querySelector('.thankyou_message');
+  const section = document.querySelector(".hero");
+  const resumeBtn = document.querySelector("#resume-link");
+  const hideCv = document.querySelector("#resume-close");
+  const sections = document.querySelectorAll(".section-animate");
 
-    if (projectPosition < screenPosition) {
-      project.classList.add("animate__fadeInUp");
-    }
-  });
-});
-//back to top btn
-const backToTopBtn = document.querySelector("#backToTopBtn");
+  // Function to handle the "Back to Top" button
+  const backToTop = () => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 20) {
+        backToTopBtn.classList.remove('d-none');
+        backToTopBtn.classList.add('d-block');
+      } else {
+        backToTopBtn.classList.remove('d-block');
+        backToTopBtn.classList.add('d-none');
+      }
+    });
 
-const backToTop = () => {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 20) {
-      backToTopBtn.classList.remove('d-none');
-      backToTopBtn.classList.add('d-block');
-    } else {
-      backToTopBtn.classList.remove('d-block');
-      backToTopBtn.classList.add('d-none');
-    }
+    backToTopBtn.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  };
 
-  });
-
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
-};
-
-// Poziv funkcije za "Back to Top" dugme
-backToTop();
-//smooth scrolling
-
-const navLinks = document.querySelectorAll(".nav-link");
-
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
+  // Function to handle smooth scrolling for navigation links
+  const smoothScroll = (e) => {
     e.preventDefault();
     const target = e.target.getAttribute("href");
     document.querySelector(target).scrollIntoView({
       behavior: "smooth",
     });
+  };
+
+  // Function to handle form submission and thank you message
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    form.style.display = 'none';
+    thankYouMessage.style.display = 'block';
+  };
+
+  // Function to reveal sections when scrolling down
+  const revealSections = () => {
+    sections.forEach((section) => {
+      const sectionPosition = section.getBoundingClientRect().top;
+      const screenPosition = window.innerHeight / 1.3;
+
+      if (sectionPosition < screenPosition) {
+        section.classList.add("animate__fadeInUp");
+      }
+    });
+  };
+
+  // Function to reveal the home section with a delay
+  const revealHomeSection = () => {
+    setTimeout(() => {
+      section.classList.remove("hidden");
+      section.classList.add("animate__fadeInLeft");
+    }, 2000);
+  };
+
+  // Function to show and hide the resume section
+  const toggleResumeSection = () => {
+    const resumeSection = document.querySelector("#resume");
+
+    resumeBtn.addEventListener("click", () => {
+      resumeSection.classList.remove("hidden");
+      resumeSection.classList.add("animate__fadeInLeft");
+    });
+
+    hideCv.addEventListener("click", () => {
+      resumeSection.classList.remove("animate__fadeInLeft");
+      resumeSection.classList.add("animate__fadeOutRight");
+      resumeSection.classList.add("hidden");
+    });
+  };
+
+  // Add event listeners
+  backToTop();
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", smoothScroll);
   });
+  form.addEventListener('submit', handleSubmit);
+  revealHomeSection();
+  toggleResumeSection();
+  window.addEventListener("scroll", revealSections);
 });
 
-const form = document.querySelector('.contact-form');
-const thankYouMessage = document.querySelector('.thankyou_message');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  form.style.display = 'none';
-  thankYouMessage.style.display = 'block';
-});
-
-// home section load with delay 
-
-const section = document.querySelector(".hero");
-
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    section.classList.remove("hidden");
-    section.classList.add("animate__fadeInLeft");
-  }, 2000);
-
-});
-
-// show resume section on click
-
-const resumeBtn = document.querySelector("#resume-link");
-const hideCv = document.querySelector("#resume-close");
-
-resumeBtn.addEventListener("click", () => {
-  const resumeSection = document.querySelector("#resume");
-  resumeSection.classList.remove("hidden");
-  resumeSection.classList.add("animate__fadeInLeft");
-});
-
-hideCv.addEventListener("click", () => {
-  const resumeSection = document.querySelector("#resume");
-  resumeSection.classList.remove("animate__fadeInLeft");
-  resumeSection.classList.add("animate__fadeOutRight");
-  resumeSection.classList.add("hidden");
-});
-
-// sections reveal when scrolling down
-
-const sections = document.querySelectorAll(".section-animate");
